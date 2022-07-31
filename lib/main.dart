fc440a43fe6dddbefbeeb980fc79fdbe232a269f
chaitrapr365 <92593443+chaitrapr365@users.noreply.github.com>
2022-07-31 11:24:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:miniproject/ui/auth.dart';
 
-import 'ui/auth.dart';
+import 'package:miniproject/loginpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    (
+    // Replace with actual values
+    options: FirebaseOptions(
+      apiKey: "XXX",
+      appId: "XXX",
+      messagingSenderId: "XXX",
+      projectId: "XXX",
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -16,8 +26,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nutrix',
-     
+      title:'Nurtrix',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
       home: Auth(),
     );
   }
