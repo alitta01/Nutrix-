d1156c1dc3a51517e29dfc37fb628ac25d4278cd
chaitrapr365 <92593443+chaitrapr365@users.noreply.github.com>
2022-08-01 01:02:45 +0530

    Update swapage.dart

--- lib/swapage.dart
+++ lib/swapage.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'package:intl/intl.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,12 +18,55 @@ class _Swapf extends State<Swapf> {
   Widget build(BuildContext context) {
     return Container(
       child: Text(week.toString()),
+    
     );*/
-    TextEditingController dateInput = TextEditingController();
+DateTime findFirstDateOfTheWeek(DateTime dateTime) {
+  return dateTime.subtract(Duration(days: dateTime.weekday - 1));
+}
+DateTime findLastDateOfTheWeek(DateTime dateTime) {
+  if(dateTime.weekday!=7){
+    return dateTime
+        .add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));}
+  else{
+    DateTime s1=dateTime
+        .add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));
+        return s1.add(Duration(days:7));
+  }
+}
+int findDay(DateTime dateTime) {
+    return dateTime.day;
+}
+int findMonth(DateTime dateTime) {
+    return dateTime.month;
+}
+int findYear(DateTime dateTime) {
+    return dateTime.year;
+}
+
+DateTime today=DateTime.now();
+late DateTime first;
+late DateTime last;
+late DateTime SelectDate;
+late String SelectShift;
+
+TextEditingController dateInput = TextEditingController();
  
+String dropdownvalue = 'Select Shift';  
+ 
+  // List of items in our dropdown menu
+  var items = [   
+    'Select Shift',
+    'Day',
+    'Afternoon',
+    'Night',
+  ];
+
   @override
   void initState() {
     dateInput.text = ""; //set the initial value of text field
+    DateTime today=DateTime.now();
+    first=findFirstDateOfTheWeek(today);
+    last=findLastDateOfTheWeek(today);
     super.initState();
   }
     @override
@@ -33,40 +77,46 @@ class _Swapf extends State<Swapf> {
           backgroundColor: Colors.redAccent, //background color of app bar
         ),
         body: Container(
-            padding: EdgeInsets.all(15),
-            height: MediaQuery.of(context).size.width / 3,
-            child: Center(
-                child: TextField(
-              controller: dateInput,
-              //editing controller of this TextField
-              decoration: InputDecoration(
-                  icon: Icon(Icons.calendar_today), //icon of text field
-                  labelText: "Enter Date" //label text of field
-                  ),
-              readOnly: true,
-              //set it true, so that user will not able to edit text
-              onTap: () async {
-                DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2021),
-                    //DateTime.now() - not to allow to choose before today.
-                    lastDate: DateTime(2022));
- 
-                if (pickedDate != null) {
-                  print(
-                      pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
-                  String formattedDate =DateFormat('yyyy-MM-dd').format(pickedDate);
-                  print(
-                      formattedDate); //formatted date output using intl package =>  2021-03-16
-                  setState(() {
-                    dateInput.text =formattedDate; //set output date to TextField value.
-                  });
-                } else {}
+          child:Column(
+            children:[
+              TextButton(
+                onPressed: () {
+                  DatePicker.showDatePicker(context,
+                  showTitleActions: true,
+                  minTime: DateTime(findYear(first),findMonth(first),findDay(first)),
+                  maxTime: DateTime(findYear(last),findMonth(last),findDay(last)), onChanged: (date) {
+                  print('change $date');
+                  }, onConfirm: (date) {
+                  print('confirm $date');
+                  SelectDate=DateFormat.ABBR_MONTH_DAY.format(date);
+                  print(SelectDate);
+                  }, currentTime: DateTime.now(), locale: LocaleType.en);
+                  },
+                  child: Text('Select date',style: TextStyle(color: Colors.blue),)),
+                 Center(
+                    child:DropdownButton(
+                      // Initial Value    
+                      value: dropdownvalue,
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),   
+                      // Array list of items
+                      items: items.map((String items) {
+                return DropdownMenuItem(
+                    value: items,
+                    child: Text(items),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                  SelectShift=newValue;//
+                });
               },
-            ))));
+            ),
+                  )]
+
+          )
+        )
+        );
   }
-}
-class Timepicker extends StatelessWidget{
-  return 
 }
