9fe73957453b435bfbcf52c1aaba8ec4a8f757c9
chaitrapr365 <92593443+chaitrapr365@users.noreply.github.com>
2022-08-02 06:43:40 +0530

    Update addschedule.dart

--- lib/addschedule.dart
+++ lib/addschedule.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
-
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class NewSchedule extends StatefulWidget {
   @override
@@ -10,11 +10,86 @@ class NewSchedule extends StatefulWidget {
 }
 
 class _NewSchedule extends State<NewSchedule> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('SCHEDULE'),
-    );
+
+  String dropdownvalue1 = 'Select Shift';  
+ 
+  // List of items in our dropdown menu
+  var Shiftitems = [   
+    'Select Shift',
+    'Day',
+    'Afternoon',
+    'Night',
+  ];
+  
+  String dropdownvalue2 = 'Select Day';
+  var Dayitems = [   
+    'Select Day',
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+  String SelectDay='';
+  String SelectShift='';
+ @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Adding Schedule"),
+          backgroundColor: Colors.greenAccent, //background color of app bar
+        ),
+        body: Container(
+          child:Column(
+            children:[
+              Center(
+                    child:DropdownButton(
+                      // Initial Value    
+                      value: dropdownvalue2,
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),   
+                      // Array list of items
+                      items: Dayitems.map((String Dayitems) {
+                return DropdownMenuItem(
+                    value: Dayitems,
+                    child: Text(Dayitems),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue2 = newValue!;
+                  SelectDay=newValue;
+                });
+              },
+            ),),
+            Center(
+              child:DropdownButton(
+                      // Initial Value    
+                      value:dropdownvalue1,
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),   
+                      // Array list of items
+                      items: Shiftitems.map((String Shiftitems) {
+                return DropdownMenuItem(
+                    value: Shiftitems,
+                    child: Text(Shiftitems),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue1 = newValue!;
+                  SelectShift=newValue;//
+                });
+              },
+            ),
+                  )]
+
+          )
+        )
+        );
+    
   }
 
 }
